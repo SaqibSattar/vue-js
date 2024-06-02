ffe086eea370d1dba7672147d2f634d9b00fea4a
SaqibSattar <Saqibkhanz1234@gmail.com>
2024-06-02 16:47:16 +0500

    html binding

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,14 +3,28 @@ export default {
   name: 'App',
   data() {
     return {
+      name: 'Saqi',
+      greet: 'Hello',
+      channel: '<b>Youtube</b>',
+      hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!<a/>`,
       headingId: 'heading',
       isDisabled: true
     }
   }
 }
 </script>
+
 // two ways to bind text // 1. mustache syntax // 2. v-text attribute
+
 <template>
+  <!-- Binding Text -->
+  <div>{{ greet }} {{ name }}</div>
+
+  <!-- Binding HTML -->
+  <div v-html="channel"></div>
+  <div v-html="hack"></div>
+
+  <!-- Binding Attributes -->
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
 </template>
