d5ef5d347f1dcf44ffd7cb55e3a7686361d9a11d
SaqibSattar <Saqibkhanz1234@gmail.com>
2024-06-02 18:14:51 +0500

    class binding

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -8,7 +8,10 @@ export default {
       channel: '<b>Youtube</b>',
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a prize!<a/>`,
       headingId: 'heading',
-      isDisabled: true
+      isDisabled: true,
+      status: 'danger',
+      isPromoted: false,
+      isSoldOut: true
     }
   }
 }
@@ -27,6 +30,39 @@ export default {
   <!-- Binding Attributes -->
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+
+  <!-- Binding Classes -->
+  <h2 class="underline">Underlined Text</h2>
+  <h2 v-bind:class="status">Status</h2>
+  <h2 class="underline" v-bind:class="status">Static Dynamic</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? Movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut
+    }"
+  >
+    Object Conditional Movie
+  </h2>
 </template>
 
-<style scoped></style>
+<style scoped>
+.underline {
+  text-decoration: underline;
+}
+.promoted {
+  font-style: italic;
+}
+.new {
+  color: olivedrab;
+}
+.sold-out {
+  color: red;
+}
+</style>
