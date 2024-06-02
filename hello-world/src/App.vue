edb48b6b7abef0bb5ce9761798737272701cc272
SaqibSattar <Saqibkhanz1234@gmail.com>
2024-06-02 16:30:25 +0500

    attribute binding

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,46 +3,16 @@ export default {
   name: 'App',
   data() {
     return {
-      userName: 'Saqi',
-      greet: 'Hi',
-      website: 'Youtube'
+      headingId: 'heading',
+      isDisabled: true
     }
   }
 }
 </script>
 // two ways to bind text // 1. mustache syntax // 2. v-text attribute
 <template>
-  <div class="wrapper">
-    <div>{{ greet }}, {{ userName }}</div>
-    <div v-text="website"></div>
-  </div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Bind</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
